090f07c9dae13f66dc7d1b300c5d0ab3585234c8
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2024-10-20 13:42:36 +0700

    Khởi tạo dự án

--- user-service/src/main/java/com/user_service/config/JwtTokenFilter.java
+++ user-service/src/main/java/com/user_service/config/JwtTokenFilter.java
@@ -1,6 +1,8 @@
 package com.user_service.config;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -37,10 +39,23 @@ import java.util.ArrayList;
                 UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
-        } catch (Exception e) {
-            // Ghi lại lỗi hoặc gửi thông báo lỗi cho client
+        } catch (ExpiredJwtException e) {
+            // Xử lý token hết hạn
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 Unauthorized
-            response.getWriter().write("Token expired or invalid!");
+            response.setContentType("text/plain; charset=UTF-8");
+            response.getWriter().write("Token đã hết hạn!"); // Gửi thông báo hết hạn
+            return; // Ngăn chuyển tiếp yêu cầu
+        } catch (SignatureException e) {
+            // Xử lý token không hợp lệ
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("text/plain; charset=UTF-8");
+            response.getWriter().write("Token không hợp lệ!"); // Gửi thông báo không hợp lệ
+            return; // Ngăn chuyển tiếp yêu cầu
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("text/plain; charset=UTF-8");
+            response.getWriter().write("Đã xảy ra lỗi với token!"); // Gửi thông báo lỗi chung
             return; // Ngăn chuyển tiếp yêu cầu
         }
 
